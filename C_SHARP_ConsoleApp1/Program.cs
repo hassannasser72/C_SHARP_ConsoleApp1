df2b7ad9570169beccc8fbeaf0e29f445f841375
hassannasser72 <hnasser@group.local>
2021-03-17 16:35:07 +0100

    Initial commit

---
+++ C_SHARP_ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace C_SHARP_ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            Console.WriteLine("Hello World!");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("x cube is:{0}", computeCube(x));
+        }
+        static int computeCube(int x)
+        {
+            return x * x * x;
+        }
+    }
+}
